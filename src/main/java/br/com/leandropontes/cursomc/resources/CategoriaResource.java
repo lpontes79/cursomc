5e69b8f651a4ae25f1c281e15d5228fd9def8042
Leandro Pontes <leandro.bh@gmail.com>
2020-07-31 10:17:30 -0300

    Banco de dados H2 e criação automática da base de dados.

--- src/main/java/br/com/leandropontes/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/com/leandropontes/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,15 @@
 package br.com.leandropontes.cursomc.resources;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 	
-	@RequestMapping(method = RequestMethod.GET)
-	//@GetMapping
+	//@RequestMapping(method = RequestMethod.GET)
+	@GetMapping
 	public String listar() {
 		return "Este método está funcionando!";
 	}
